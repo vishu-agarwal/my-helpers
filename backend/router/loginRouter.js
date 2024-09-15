6008065a3dfbc0b4f76c6927408fb5aaa5d75343
vishu-agarwal <vishuagarwaldec@gmail.com>
2024-09-15 15:34:38 +0530

    added initial route /

--- backend/router/loginRouter.js
+++ backend/router/loginRouter.js
@@ -1,6 +1,8 @@
 const express = require("express")
 const router = new express.Router()
 
+router.get("",(req,res)=>res.status(200).send("Welcome to the my helpers app!"))
+
 //controllers
 const loginController = require("../controller/loginController")
 
